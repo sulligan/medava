9b5b5acdaaa2b82714077c480b414305f662d934
Nathan Sullivan <145160389+sulligan@users.noreply.github.com>
2023-11-20 17:13:10 -0500

    Add files via upload

--- src/main/java/edu/uc/cs3003/medava/Shippable.java
+++ src/main/java/edu/uc/cs3003/medava/Shippable.java
@@ -1,1 +1,6 @@
 package edu.uc.cs3003.medava;
+public interface Shippable {
+  public MedicineSchedule getSchedule();
+  public String getMedicineName();
+  public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature);
+}
